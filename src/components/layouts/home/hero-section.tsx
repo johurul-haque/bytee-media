7a5ee858ca390be42036c5554a9392f84287af92
Johurul Haque <johurulhaquejony@gmail.com>
2024-06-21 23:55:13 +0600

    updated hero section

--- src/components/layouts/home/hero-section.tsx
+++ src/components/layouts/home/hero-section.tsx
@@ -6,19 +6,19 @@ import { BookACall } from "@/components/book-a-call";
 export function HeroSection() {
   return (
     <section className="flex max-lg:flex-col items-center justify-between lg:h-[clamp(35rem,90svh,45rem)]">
-      <div className="mt-20 mb-10">
-        <h1 className="text-3xl xs:text-4xl md:text-5xl xs:leading-[2.8rem] xs:tracking-wide font-medium">
+      <div className="mt-20 mb-10 max-lg:text-center">
+        <h1 className="text-3xl xs:text-4xl sm:text-5xl xs:leading-[2.8rem] xs:tracking-wide font-medium">
           Tired of editing?
           <br />
           <TextGradient>Focus on</TextGradient> creating
         </h1>
 
-        <p className="max-w-sm mt-4 mb-6 max-xs:text-sm tracking-wider">
+        <p className="max-w-xs xs:max-w-sm mt-4 mb-6 max-xs:text-sm tracking-wider">
           One Stop Solution for content creators. Say goodbye to average content
           and {"Hello to excellence"}
         </p>
 
-        <BookACall />
+        <BookACall className="max-lg:mx-auto" />
       </div>
 
       <Image
