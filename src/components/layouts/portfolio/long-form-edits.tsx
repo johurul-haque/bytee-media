5775a523751ff27c294d5ea4c0d4666a1d419046
Johurul Haque <johurulhaquejony@gmail.com>
2024-06-26 12:34:53 +0600

    Added Ali Abdal style section

--- src/components/layouts/portfolio/long-form-edits.tsx
+++ src/components/layouts/portfolio/long-form-edits.tsx
@@ -1,5 +1,6 @@
 import imanGadhzhi from "@/assets/portfolio/cursive-text/iman-gadhzhi.png";
 import { CursiveText } from "@/components/cursive-text";
+import { AliAbdalStyle } from "@/components/layouts/portfolio/ali-abdal-style";
 
 export function LongFormEdits() {
   return (
@@ -8,29 +9,33 @@ export function LongFormEdits() {
         Long Form Video Edits
       </h2>
 
-      <CursiveText title="Iman Gadhzhi Style" textImage={imanGadhzhi} />
+      <section>
+        <CursiveText title="Iman Gadhzhi Style" textImage={imanGadhzhi} />
 
-      <div className="flex flex-wrap gap-6 justify-center mt-4">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/5SClNKuZ9-w?si=xCFnauuenzVYkoFO"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        <div className="flex flex-wrap gap-6 justify-center mt-4">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/5SClNKuZ9-w?si=xCFnauuenzVYkoFO"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
 
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/KWGgABbcYRs?si=iwBle4rj0aSsPWa8"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-      </div>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/KWGgABbcYRs?si=iwBle4rj0aSsPWa8"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </section>
+
+      <AliAbdalStyle />
     </section>
   );
 }
